3350cacc5912b53b9d6b0cfd0bc6df65f0e009ea
Mark McDermid <mark.mcdermid@leighton.com>
2016-10-03 23:02:57 +0100

    Add some fake data

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const nowPlaying = {
+  artist: 'Jay Z',
+  title: 'Dirt Off Your Shoudler',
+  requestedBy: 'Aaron',
+  img: 'jayz.jpg'
+};
+
+export const queue = [
+  {
+    id: 1,
+    artist: 'Beyonce',
+    img: 'beyonce.jpg',
+    title: 'Formation',
+    requestedBy: 'Kate'
+  },
+  {
+    id: 2,
+    artist: 'Mumford & Sons',
+    img: 'mumford.jpg',
+    title: 'Winter Winds',
+    requestedBy: 'Aaron'
+  },
+  {
+    id: 3,
+    artist: 'Miley Cyrus',
+    img: 'miley.jpg',
+    title: 'We Can\'t Stop',
+    requestedBy: 'Bigt'
+  },
+  {
+    id: 4,
+    artist: 'DNCE',
+    img: 'dnce.jpg',
+    title: 'Toothbrush',
+    requestedBy: 'Ashley'
+  },
+  {
+    id: 5,
+    artist: 'Tove Lo',
+    img: 'tove.jpg',
+    title: 'Cool Girl',
+    requestedBy: 'Scott'
+  },
+  {
+    id: 6,
+    artist: 'The Chainsmokers',
+    img: 'roses.jpg',
+    title: 'Roses',
+    requestedBy: 'Aaron'
+  },
+];
