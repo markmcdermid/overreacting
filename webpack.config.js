34748595097ccbeef3aea73af193a913a56cd78e
Mark McDermid <mark.mcdermid@leighton.com>
2016-10-03 13:12:27 +0100

    Add hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
         filename: 'bundle.js'
     },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
     ]
 };
