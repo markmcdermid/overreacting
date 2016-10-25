5c7cb0ecc2693b87db5905bb223be7ca3ddf6ea4
Mark McDermid <mark.mcdermid@leighton.com>
2016-10-25 21:31:43 +0100

    Begin Playlist Switching component

--- src/components/JukeboxApp/AdminPanel/AdminPanel.js
+++ src/components/JukeboxApp/AdminPanel/AdminPanel.js
@@ -1,10 +1,34 @@
 import React, { PropTypes } from 'react';
-import { connect } from 'react-redux';
+
+import PlaylistSwitching from './PlaylistSwitching';
 
 const AdminPanel = (props) => (
   <section className="AdminPanel">
     <div className="component__inner">
       <h1 className="h1">Admin Panel</h1>
+
+      <PlaylistSwitching />
+      <h2 className="h2">
+        Add Tracks
+      </h2>
+      <h2 className="h2">
+        Explicit Tracks
+      </h2>
+      <h2 className="h2">
+        Max in queue per IP
+      </h2>
+      <h2 className="h2">
+        Volume Slider
+      </h2>
+      <h2 className="h2">
+        Add Playlist (Category / Spotify URI / Submit)
+      </h2>
+      <h2 className="h2">
+        Queue w/ Remove Track
+      </h2>
+      <h2 className="h2">
+        Playlists w/ Remove Playlist
+      </h2>
     </div>
   </section>
 );
---
+++ src/components/JukeboxApp/AdminPanel/PlaylistSwitching.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Checkbox from 'rc-checkbox';
+
+const PlaylistSwitching = (props) => (
+
+  <article className="PlaylistSwitching">
+    <h2 className="h2">Playlist Switching</h2>
+    <Checkbox />
+  </article>
+);
+
+export default PlaylistSwitching;
